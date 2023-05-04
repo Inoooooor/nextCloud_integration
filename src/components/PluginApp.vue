5acdc4f5246688dff8cc8623130cbbf8de1815da
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-05-04 14:56:50 +0300

    messing_with_file_send

--- src/components/PluginApp.vue
+++ src/components/PluginApp.vue
@@ -1,7 +1,54 @@
-<script setup></script>
+<script setup>
+import HDE from '../plugin'
+import { ref, watch } from 'vue'
+
+const reader = new FileReader()
+
+const fileToUpload = ref(null)
+
+// const logFile = (event) => console.log(event.target.files)
+
+const updateFile = (event) => {
+  fileToUpload.value = event.target.files[0]
+  reader.readAsArrayBuffer(fileToUpload.value)
+
+  console.log('file ->', document.querySelector('#file'))
+}
+
+reader.onload = async () => {
+  const arrayBuffer = reader.result
+
+  const options = {
+    url: 'https://data.tinkoff.ru/remote.php/dav/files/HDE_cloud/testDir/testUploadForm.txt',
+    auth: 'hdeCloud',
+    method: 'PUT',
+    contentType: 'application/octet-stream',
+    headers: {
+      'Content-Length': arrayBuffer.byteLength.toString(),
+    },
+    data: arrayBuffer,
+  }
+  try {
+    const { data } = await HDE.request(options)
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+reader.onerror = (error) => {
+  console.error(error)
+}
+
+watch(fileToUpload, (newValue) => console.log(newValue))
+</script>
 
 <template>
-  <h1>Hello pluginЫЫЫ!</h1>
+  <form id="cloudForm">
+    <label for="file">В облако </label>
+    <input id="file" type="file" name="file" @change="updateFile($event)" />
+    <button @click.stop="uploadFile()">отправить</button>
+  </form>
 </template>
 
 <style lang="scss">
